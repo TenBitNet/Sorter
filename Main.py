9f2e25c18a03eeb9df3fa99c3c87c92f536dea4d
Ten Bit <10bitdirect@gmail.com>
2020-04-03 19:34:51 -0400

    no message

--- Main.py
+++ Main.py
@@ -1,7 +1,16 @@
+import os
 from winreg import *
+
+
 def DownloadsPath():
 
     with OpenKey(HKEY_CURRENT_USER, 'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders') as key:
         return QueryValueEx(key, '{374DE290-123F-4565-9164-39C4925E467B}')[0]
 
+
+directory = os.fsencode(DownloadsPath())
+
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    print(DownloadsPath() + '\\' + filename)
 
